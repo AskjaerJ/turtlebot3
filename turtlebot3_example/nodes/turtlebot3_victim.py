d421039c66bbef9d7d86f1168e02860b3eae37ed
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2023-05-10 17:33:56 +0200

    time sleep no ros slepp

---
+++ turtlebot3_example/nodes/turtlebot3_victim.py
@@ -0,0 +1,8 @@
+import rospy
+import math
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+import time  # time module
+import smbus  # led sensor
+from nav_msgs.msg import Odometry  # odom
+
